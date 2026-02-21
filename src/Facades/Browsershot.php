34dd2d64472eb6134b4b7ed3afbee0e7fb712764
Eduardo Ribeiro <eduribeiro.films@gmail.com>
2026-02-21 15:15:12 -0400

    Fix: facade intelisense

--- src/Facades/Browsershot.php
+++ src/Facades/Browsershot.php
@@ -6,7 +6,7 @@ use EduardoRibeiroDev\Browsershot\Services\BrowsershotService;
 use Illuminate\Support\Facades\Facade;
 
 /**
- *    @method static EduardoRibeiroDev\Browsershot\Services\BrowsershotService make(Illuminate\Contracts\View\View|string $content, array $data = [])
+ *    @method static \EduardoRibeiroDev\Browsershot\Services\BrowsershotService make(Illuminate\Contracts\View\View|string $content, array $data = [])
  */
 class Browsershot extends Facade
 {
